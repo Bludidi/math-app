dc797195a122432222fef91bc09a6e9acc0a893b
Bludidi <bludidi5@gmail.com>
2022-11-10 16:10:00 +0200

    declare handleClick inside Calculator function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-const handleClick = (event, state, setter) => {
-  const buttonName = event.target.innerText;
-  const result = calculate(state, buttonName);
-  setter(result);
-}
-
 const Calculator = () => {
   const [state, setter] = useState({ total: 0, operation: null, next: null });
   const { total, next, operation } = state;
+
+  const handleClick = (event, state, setter) => {
+    const buttonName = event.target.innerText;
+    const result = calculate(state, buttonName);
+    setter(result);
+  };
 
   return (
     <div className="container">
